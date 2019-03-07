bae2b052bafc2fb22cbe5b65693aefd5585afcda
viddu92 <viddu92@gmail.com>
2019-03-07 15:55:45 +0530

    adding A to master

---
+++ src/AA.java
@@ -0,0 +1,21 @@
+
+public class AA {
+	
+	void test()
+	{
+		int a = 3,b=1;
+		int j=0;
+		
+		j=a+b;
+		System.out.println("Value is " +j);
+	
+	}
+
+	public static void main(String [] args)
+	{
+		AA a=new AA();
+		a.test();
+	
+	}
+	
+}
